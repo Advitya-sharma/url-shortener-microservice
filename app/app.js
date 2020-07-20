0bb8777def5d65e1e5b7290c5a1bade331e97099
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-07-20 20:05:28 +0530

    adding schema

--- app/app.js
+++ app/app.js
@@ -3,19 +3,29 @@
 var express = require("express");
 var mongo = require("mongodb");
 var mongoose = require("mongoose");
-
 var cors = require("cors");
-
 var app = express();
+require("dotenv").config();
 
 // Basic Configuration
 var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/
-
-// mongoose.connect(process.env.DB_URI);
-
 app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+
+const urlObjSchema = new Schema({
+  url : {type:String},
+  shortUrl : {type:String}
+});
+
+const urlObj = mongoose.model("urlObj",urlObjSchema);
+
+
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -25,6 +35,13 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
+
+
+
+
+
+
+
 
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
