733d84f6859c50b3bd224bf4f1bbc88986329f55
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-07-21 05:21:58 +0530

    creating and accessing url

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ var express = require("express");
 var mongo = require("mongodb");
 var mongoose = require("mongoose");
 var cors = require("cors");
+var bodyParser = require("body-parser");
 var app = express();
 require("dotenv").config();
 
@@ -13,22 +14,17 @@ app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 const Schema = mongoose.Schema;
 
 const urlObjSchema = new Schema({
-  url : {type:String},
-  shortUrl : {type:String}
+  url: { type: String },
+  urlId: { type: Number }
 });
 
-const urlObj = mongoose.model("urlObj",urlObjSchema);
-
-
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+const urlObj = mongoose.model("urlObj", urlObjSchema);
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
@@ -36,16 +32,37 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
-
-
-
-
-
-// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  res.json({ id: req.params.id });
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const getOrCreate = (url, res) => {
+  urlObj.findOne({ url: url }, (err, data) => {
+    if (err) return console.error(err);
+    if (data) {
+      res.json({ original_url: url, short_url: data.urlId });
+    } else {
+      urlObj.create(
+        { url: url, urlId: Math.floor(Math.random() * 1000) },
+        (err, data) => {
+          if (err) return console.error(err);
+          res.json({ original_url: url, short_url: data.urlId });
+        }
+      );
+    }
+  });
+};
+
+app.post("/api/shorturl/new", (req, res) => {
+  const url = req.body.url;
+  getOrCreate(url, res);
 });
 
 app.listen(port, function () {
